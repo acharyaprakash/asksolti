cb0336d2cc686d8b3806aa69e32af30f58a3641b
Prakash Acharya <prakashacharya@Prakashs-MBP.fios-router.home>
2020-01-29 21:14:25 -0500

    image path in ts

--- src/app/plumber/plumber.component.ts
+++ src/app/plumber/plumber.component.ts
@@ -9,14 +9,14 @@ export class PlumberComponent implements OnInit {
   plumbers=[];
   constructor() { 
     this.plumbers=[
-      {Id: 1, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 5},
-      {Id: 2, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 2},
-      {Id: 3, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 3},
-      {Id: 4, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 3.5},
-      {Id: 5, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 1.5},
-      {Id: 6, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 1},
-      {Id: 7, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 2.5},
-      {Id: 8, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 4.5}
+      {Id: 1, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 5},
+      {Id: 2, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 2},
+      {Id: 3, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 3},
+      {Id: 4, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 3.5},
+      {Id: 5, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 1.5},
+      {Id: 6, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 1},
+      {Id: 7, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 2.5},
+      {Id: 8, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 4.5}
     ];
   }
 
@@ -25,7 +25,7 @@ export class PlumberComponent implements OnInit {
 
   searchPlumbers(){
     let result=[
-      {Id: 1, Name: "Prakash Acharya", Description: "Plumbing", Image:"../../assets/images/Plumber.png", Rating: 5}
+      {Id: 1, Name: "Prakash Acharya", Description: "Plumbing", Image:"././assets/images/plumber.png", Rating: 5}
     ];
 
     this.plumbers=result;
